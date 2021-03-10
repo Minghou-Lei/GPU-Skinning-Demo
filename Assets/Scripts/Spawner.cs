ad6f3e8dd44a864f1e6d5fbc9764dc766db9a6e1
Minghou Lei <36237320+Minghou-Lei@users.noreply.github.com>
2021-03-10 17:27:57 +0800

    Random Animation Clip  Implements

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
@@ -8,9 +9,12 @@ public class Spawner : MonoBehaviour
     public Material bakedMaterial;
     private MaterialPropertyBlock mpb;
     private GameObject spawn;
-
+    public List<Material> materials;
+    private System.Random random;
+    
     private void Awake()
     {
+        random = new System.Random();
         spawn = new GameObject();
         spawn.name = "spawned";
         spawn.transform.parent = transform;
@@ -29,13 +33,17 @@ public class Spawner : MonoBehaviour
         for (var c = 0; c < column; c++)
         {
             var rc = Instantiate(spawn);
+            MeshRenderer mr = rc.GetComponent<MeshRenderer>();
+            int i = random.Next(materials.Count);
+            mr.material = materials[i];
             var mpb = new MaterialPropertyBlock();
             rc.transform.parent = transform;
             rc.transform.position += new Vector3(r, 0, c);
-            //rc.GetComponent<MeshRenderer>().material.SetFloat("_Random",Random.Range(0f,1f));
-            rc.GetComponent<MeshRenderer>().GetPropertyBlock(mpb);
+            rc.gameObject.name = mr.name;
+            
+            mr.GetPropertyBlock(mpb);
             mpb.SetFloat("_Offset", Random.Range(0f, 1f));
-            rc.GetComponent<MeshRenderer>().SetPropertyBlock(mpb);
+            mr.SetPropertyBlock(mpb);
         }
     }
 }
